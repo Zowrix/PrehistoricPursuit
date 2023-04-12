c438ebdcd83f1029d89a685ef8eafedc8c6684f4
Geoffrey Havard <havardgeoffrey@gmail.com>
2023-04-12 11:49:16 -0400

    Double Saut

--- Assets/Scriptes/Personnage/Hero.cs
+++ Assets/Scriptes/Personnage/Hero.cs
@@ -7,6 +7,14 @@ public class Hero : MonoBehaviour
 
     [SerializeField] private float _vitesseMarche = 1f;
     [SerializeField] private float _vitesseCourse = 5f;
+    [SerializeField] private float _puissanceSaut = 7f;
+    [SerializeField] private float _doubleSautPower = 8f;
+
+    [SerializeField] private Transform _groundCheck;
+    [SerializeField] private LayerMask _presenceSol;
+
+
+
 
 
     private Rigidbody2D _body;
@@ -14,6 +22,10 @@ public class Hero : MonoBehaviour
     private float _hDirection = 0;
 
     private bool _courir = false;
+    private bool _sauter = false;
+    [SerializeField] private bool _solPrincipale = false;
+
+    [SerializeField] private bool _doubleSaut;
 
 
     // Start is called before the first frame update
@@ -25,15 +37,41 @@ public class Hero : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Collider2D collider = Physics2D.OverlapCircle(_groundCheck.position, 0.25f, _presenceSol);
+
+        _solPrincipale = collider != null;
+
+
         _hDirection = Input.GetAxisRaw("Horizontal");
 
         _courir = Input.GetAxisRaw("Courir") != 0;
 
-
+        _sauter = Input.GetAxisRaw("Sauter") != 0;
+        Debug.Log(_sauter);
 
         if (_hDirection != 0)
         {
             transform.localScale = new Vector3(_hDirection, 1, 1);
+        }
+
+        if (_solPrincipale && !Input.GetButtonDown("Sauter"))
+        {
+            _doubleSaut = false;
+        }
+
+        if (Input.GetButtonDown("Sauter"))
+        {
+            if (_solPrincipale)
+            {
+                _body.velocity = new Vector2(_body.velocity.x, _doubleSaut ? _doubleSautPower : _puissanceSaut);
+                _doubleSaut = !_doubleSaut;
+                Debug.Log("doubleSaut");
+            }
+        }
+
+        if (Input.GetButtonDown("Sauter") && _body.velocity.y > 0f)
+        {
+            _body.velocity = new Vector2(_body.velocity.x, _body.velocity.y * 0.5f);
         }
     }
 
@@ -48,6 +86,9 @@ public class Hero : MonoBehaviour
             _body.velocity = new Vector2(_vitesseCourse * _hDirection, _body.velocity.y);
 
         }
+
+
+
     }
 
 }
